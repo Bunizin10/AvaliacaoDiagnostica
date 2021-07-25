bc559fefae2f95d5e6ca512c1b5baf9f6eb65c41
Bunizin10 <87842274+Bunizin10@users.noreply.github.com>
2021-07-25 20:30:25 -0300

    Avaliação Diagnostica - Logica de Programação
    Avaliação Diagnostica - Logica de Programação

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AvaliacaoDiagnostica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tamanho = 10;
+            int[] numeros = new int[tamanho];
+            int restoDivisao = 0;
+
+            bool encontrado = false;
+
+            Console.WriteLine("Digite 10 Valores Inteiros :");
+            for (int i = 0; i < tamanho; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite um Valor Inteiro para Procurar :");
+            int numeroProcurado = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                if (numeros[i] == numeroProcurado)
+                {
+                    encontrado = true;
+                    restoDivisao = numeroProcurado % 2;
+                    if (restoDivisao == 0)
+                    {
+                        Console.WriteLine("O número PAR {0} foi encontrado com sucesso!", numeros[i]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("O número IMPAR {0} foi encontrado com sucesso!", numeros[i]);
+                    }
+
+                }
+
+            }
+            if (encontrado == false)
+            {
+                Console.WriteLine("O número {0} não foi encontrado!", numeroProcurado);
+            }
+
+
+        }
+    }
+}
